e269cf97d01f556b5eae2a607338fed46138e860
Baruc Alejandria <alejandriacbaruc@gmail.com>
2026-06-25 02:27:13 -0500

    feat(techlogistics): add SectionTitle to Architecture section on Techlogistics

--- src/pages/projects/techlogistics/components/ArchitectureTechLogistics.tsx
+++ src/pages/projects/techlogistics/components/ArchitectureTechLogistics.tsx
@@ -2,10 +2,13 @@ import { techLogisticsProject } from '@/constants/projects/techlogistics';
 import { SectionLayout } from '@/components/SectionLayout';
 import type { BaseSectionProps } from '@/types';
 import { CustomTabs } from '@/components/CustomTabs';
+import { SectionTitle } from '@/components/SectionTitle';
+import { Layers } from 'lucide-react';
 
 export const ArchitectureTechLogistics = ({ id }: BaseSectionProps) => {
   return (
     <SectionLayout id={id}>
+      <SectionTitle icon={Layers} text="Arquitectura" />
       <div className="container mx-auto px-4 lg:px-12">
         <CustomTabs project={techLogisticsProject} />
       </div>
